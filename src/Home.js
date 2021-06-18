de191a91f48c9216ef1cbcc7b36cd00d70581499
hokagay7 <68209911+hokagay7@users.noreply.github.com>
2021-06-18 13:32:30 +0800

    change color and stuff

--- src/Home.js
+++ src/Home.js
@@ -26,25 +26,25 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
+				<div style={{fontSize: "14px", background: "#729672", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
+					Hosted by: 
+					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/hokagay7"}>
 						<GitHubIcon />
 					</IconButton>
 				</div>
 				
 				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
+					<h1 style={{ fontSize: "45px" }}>This Is Not Google Meet</h1>
+					<p style={{ fontWeight: "bold" }}>This is not Google Meet. Google can't sue me.</p>
 				</div>
 
 				<div style={{
-					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
+					background: "#729672", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
 				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Create or join a meet</p>
+					<Input placeholder="Code or URL" onChange={e => this.handleChange(e)} />
+					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Connect</Button>
 				</div>
 			</div>
 		)
